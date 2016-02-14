10c7fdcf698479e514a0e20f6752c37598bd74ef
Rafael Schmid <rafaelfschmid@gmail.com>
2016-02-14 13:30:07 -0200

    generate

--- matrix_multiplication/scripts/generate.sh
+++ matrix_multiplication/scripts/generate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 dir=$1
 
-c=2
-while [ $c -le 8192 ]
+c=32
+while [ $c -le 2048 ]
 do
 #	echo $c
-	for b in `seq 1 10`; do
+#	for b in `seq 1 10`; do
 		./equal.exe $c > $dir/$c".in"
-	done
+#	done
 	((c=$c*2))
 	sleep 1.0
 #	echo " "
