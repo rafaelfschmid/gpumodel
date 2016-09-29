8bd2c4d83f1fea872582c1d6ca9ab017a187bc77
Rafael Schmid <rafaelfschmid@gmail.com>
2016-09-29 15:41:27 -0300

    script execution

--- matrix_multiplication/config_environment.sh
+++ matrix_multiplication/config_environment.sh
@@ -3,10 +3,10 @@ export CUDA_HOME=/usr/local/cuda-7.5
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64  
 PATH=${CUDA_HOME}/bin:${PATH}
 
-export TIME=0
+export TIME=1
 export EXECS=1
 export BITS=4
-export BLOCK=2
+#export BLOCK=2
 #export BLOCK=4
 #export BLOCK=8
 #export BLOCK=16
--- matrix_multiplication/scripts/exec_all.sh
+++ matrix_multiplication/scripts/exec_all.sh
@@ -1,14 +1,15 @@
+dir=$1
 
-#./scripts/exec.sh shared2.exe ~/workspace/files/gpumodel/inputs > times-lmcad/shared2.out 
-#./scripts/exec.sh global2.exe ~/workspace/files/gpumodel/inputs > times-lmcad/global2.out
-./scripts/exec.sh shared4.exe ~/workspace/files/gpumodel/inputs > times-lmcad/shared4.out 
-#./scripts/exec.sh global4.exe ~/workspace/files/gpumodel/inputs > times-lmcad/global4.out
-#./scripts/exec.sh shared8.exe ~/workspace/files/gpumodel/inputs > times-lmcad/shared8.out 
-#./scripts/exec.sh global8.exe ~/workspace/files/gpumodel/inputs > times-lmcad/global8.out
-#./scripts/exec.sh shared16.exe ~/workspace/files/gpumodel/inputs > times-lmcad/shared16.out 
-#./scripts/exec.sh global16.exe ~/workspace/files/gpumodel/inputs > times-lmcad/global16.out
-#./scripts/exec.sh shared32.exe ~/workspace/files/gpumodel/inputs > times-lmcad/shared32.out 
-#./scripts/exec.sh global32.exe ~/workspace/files/gpumodel/inputs > times-lmcad/global32.out
+#./scripts/exec.sh shared2.exe ~/workspace/files/gpumodel/inputs > $dir/shared2.out 
+./scripts/exec.sh global2.exe ~/workspace/files/gpumodel/inputs > $dir/global2.out
+#./scripts/exec.sh shared4.exe ~/workspace/files/gpumodel/inputs > $dir/shared4.out 
+./scripts/exec.sh global4.exe ~/workspace/files/gpumodel/inputs > $dir/global4.out
+#./scripts/exec.sh shared8.exe ~/workspace/files/gpumodel/inputs > $dir/shared8.out 
+./scripts/exec.sh global8.exe ~/workspace/files/gpumodel/inputs > $dir/global8.out
+#./scripts/exec.sh shared16.exe ~/workspace/files/gpumodel/inputs > $dir/shared16.out 
+./scripts/exec.sh global16.exe ~/workspace/files/gpumodel/inputs > $dir/global16.out
+#./scripts/exec.sh shared32.exe ~/workspace/files/gpumodel/inputs > $dir/shared32.out 
+./scripts/exec.sh global32.exe ~/workspace/files/gpumodel/inputs > $dir/global32.out
 
 
 #./scripts/exec.sh shared64.exe ~/workspace/files/gpumodel/inputs > times-lmcad/shared64.out 
