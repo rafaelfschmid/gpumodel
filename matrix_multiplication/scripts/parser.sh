3a6132476b5a34ea485572690049d074f27ca548
Rafael Schmid <rafaelfschmid@gmail.com>
2017-06-28 11:29:33 -0400

    times parsed and parser block

--- matrix_multiplication/scripts/parser.sh
+++ matrix_multiplication/scripts/parser.sh
@@ -3,7 +3,8 @@ times=$1
 
 for i in 2 4 8 16 32 ; do
 	./parser.exe $times/global_bi$i.out $times/00global_bi$i.out
-	./parser.exe $times/shared$i.out $times/00shared$i.out 
+	./parser.exe $times/shared$i.out $times/00shared$i.out
+ 	./parser.exe $times/global_block$i.out $times/00global_block$i.out
 done
 
 for i in 2 4 8 16 32 64 128 256 512 1024; do
