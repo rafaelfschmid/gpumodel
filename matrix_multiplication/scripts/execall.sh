2a1434312fe3739aa8fc896bc379064c4175f085
Rafael Schmid <rafaelfschmid@gmail.com>
2017-07-19 16:34:54 -0400

    lib

--- matrix_multiplication/scripts/execall.sh
+++ matrix_multiplication/scripts/execall.sh
@@ -2,27 +2,8 @@
 input=$1 #arquivos de entrada
 time=$2 #arquivos de tempo
 
-#./scripts/exec_unidimensional.sh $input $time
-#./scripts/exec_tridimensional.sh $input $time
-
-#./scripts/exec.sh shared2.exe $input > $time/shared2.out 
-#./scripts/exec.sh global_bi2.exe $input > $time/global_bi2.out
-./scripts/exec.sh global_block2.exe $input > $time/global_block2.out
-
-#./scripts/exec.sh shared4.exe $input > $time/shared4.out 
-#./scripts/exec.sh global_bi4.exe $input > $time/global_bi4.out
-./scripts/exec.sh global_block4.exe $input > $time/global_block4.out
-
-#./scripts/exec.sh shared8.exe $input > $time/shared8.out 
-#./scripts/exec.sh global_bi8.exe $input > $time/global_bi8.out
-./scripts/exec.sh global_block8.exe $input > $time/global_block8.out
-
-#./scripts/exec.sh shared16.exe $input > $time/shared16.out 
-#./scripts/exec.sh global_bi16.exe $input > $time/global_bi16.out
-./scripts/exec.sh global_block16.exe $input > $time/global_block16.out
-
-#./scripts/exec.sh shared32.exe $input > $time/shared32.out 
-#./scripts/exec.sh global_bi32.exe $input > $time/global_bi32.out
-./scripts/exec.sh global_block32.exe $input > $time/global_block32.out
+#./scripts/execuni.sh $input $time
+./scripts/execbi.sh $input $time
+#./scripts/exectri.sh $input $time
 
 
